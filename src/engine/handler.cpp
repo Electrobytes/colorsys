c4105075bcdee3ea8940c89dbde1199911adf079
Electrobytes101 <electrobytes101@proton.me>
2025-11-06 20:22:15 +0800

    restructured tree for engine module

---
+++ src/engine/handler.cpp
@@ -0,0 +1,16 @@
+#include "engine.h"
+
+namespace colorsys {
+
+    std::vector<std::vector<int>> engineHandler(const std::vector<int>& intermediateValues, const int& selectedMode, int magnitude) {
+        switch (selectedMode) {
+            case colorsys::m_convert:
+                return { intermediateValues };
+
+            case colorsys::m_complementary:
+                return engine::complementary(intermediateValues);
+        }
+
+        return {{420}}; // There's no color model/system that would reach number 420 so that's prolly a good placeholder for a while
+    }
+}
