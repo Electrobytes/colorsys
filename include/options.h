24304f90cf937b9c5857b49c0ab318cd6f4f952e
Electrobytes101 <electrobytes101@proton.me>
2025-12-19 18:41:54 +0800

    rearranged flags and added one for color variations

--- include/options.h
+++ include/options.h
@@ -9,17 +9,21 @@ namespace colorsys {
         cxxopts::Options programFlags("colorsys", "Color Theory in the cmd!");
 
         programFlags.add_options("General arguments")
-            ("mode", "Choose what to do with the input", cxxopts::value<std::string>())
-            ("input", "Your color input", cxxopts::value<std::string>());
+            ("mode", "Choose what to do with the input.", cxxopts::value<std::string>())
+            ("input", "Your color input.", cxxopts::value<std::string>());
 
         programFlags.add_options("Color model")
-            ("i,inputModel", "Color mixing model of the input (Required)", cxxopts::value<std::string>())
+            ("i,inputModel", "Color mixing model of the input. (Required)", cxxopts::value<std::string>())
             ("o,outputModel", "Color mixing model for the output. Default value is matched to the -i,--inputModel flag, but using the \"convert\" function will make this flag the basis for the output.", cxxopts::value<std::string>());
 
-        programFlags.add_options("Additional options")
+        programFlags.add_options("Fine-tuning")
             ("m,magnitude", "The intensity/magnitude of results", cxxopts::value<int>()->default_value("0"))
-            ("h,help", "Prints the help message and exits")
-            ("d,debug", "Show unformatted results for debugging", cxxopts::value<bool>());
+            ("r,range", "The number of outputs to be displayed. Not all functions support this.", cxxopts::value<int>()->default_value("1"));
+            
+        programFlags.add_options("Additional options")
+            
+            ("h,help", "Prints the help message and exits.")
+            ("d,debug", "Show unformatted results for debugging.", cxxopts::value<bool>());
 
         programFlags.parse_positional({"mode", "input"});
 
