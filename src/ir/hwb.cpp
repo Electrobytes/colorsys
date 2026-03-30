09fa1848e86e8b419453f6d95c68eb8817d9e4db
Electrobytes101 <electrobytes101@proton.me>
2026-03-30 15:02:20 +0800

    from hwb formula fixed

--- src/ir/hwb.cpp
+++ src/ir/hwb.cpp
@@ -3,7 +3,7 @@
 namespace colorsys::intermediate {
     namespace start {
         void hwb(std::vector<int>& convert) {
-            convert.at(1) = NULL; // TODO: formula
+            convert.at(1) = (100 - convert.at(1)) * (100 - convert.at(2)) / 100; // TODO: formula
             convert.at(2) = 1 - convert.at(2);
             colorsys::intermediate::start::hsv(convert);
 
