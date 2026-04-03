c89bd81a0db873b432fb1c2644f99999e8173763
Electrobytes101 <electrobytes101@proton.me>
2026-04-03 20:40:34 +0800

    Implemented hwb color model,; to be tested

--- src/ir/hwb.cpp
+++ src/ir/hwb.cpp
@@ -3,18 +3,22 @@
 namespace colorsys::intermediate {
     namespace start {
         void hwb(std::vector<int>& convert) {
-            convert.at(1) = (100 - convert.at(1)) * (100 - convert.at(2)) / 100; // TODO: formula
-            convert.at(2) = 1 - convert.at(2);
+            convert.at(1) = 100 * (1 - convert.at(1) / (100 - convert.at(2)));
+            convert.at(2) -= 100;
             colorsys::intermediate::start::hsv(convert);
-
-
-
         }
     }
 
     namespace finish {
         void hwb(std::vector<int>& convert) {
-            // TODO: formula
+            colorsys::intermediate::finish::hsv(convert);
+            convert.at(1) = convert.at(2) - (convert.at(1) * convert.at(2) / 10);
+            convert.at(2) -= 100;
         }
     }
 }
+
+/**
+ * Reference(s):
+ * - https://alvyray.com/Papers/CG/HWB_JGTv208.pdf
+ */
